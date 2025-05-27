11700a7efcf5b8aedc1a817e3b5c4d51b45fdeaa
beyondie2 <heewoong7843@gmail.com>
2025-05-27 18:25:47 +0900

    431페이지 회원정보 수정 화면 처리리

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -3,6 +3,7 @@ const Loading = <div>Loading....</div>
 const Login = lazy(() => import("../pages/member/LoginPage"))
 const LogoutPage = lazy(() => import("../pages/member/LogoutPage"))
 const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"))
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"))
 
 const memberRouter = () => {
 
@@ -18,7 +19,11 @@ const memberRouter = () => {
     {
       path:"kakao",
       element: <Suspense fallback={Loading}><KakaoRedirect/></Suspense>,
-    }
+    },
+    {
+      path:"modify",
+      element: <Suspense fallback={Loading}><MemberModify/></Suspense>,
+    },    
   ]
   
 }

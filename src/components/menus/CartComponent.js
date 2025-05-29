4f209d23e55cfbd15fd42a2b61fc4f890ff05187
beyondie2 <heewoong7843@gmail.com>
2025-05-30 01:25:44 +0900

    473페이지 장바구니용 컴포넌트 시작부분분

---
+++ src/components/menus/CartComponent.js
@@ -0,0 +1,8 @@
+const CartComponent = () => {
+  return (
+    <div className="w-full">
+      <div>Cart</div>
+    </div>
+  );
+}
+export default CartComponent;
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,18 +1,22 @@
 import BasicMenu from "../components/menus/BasicMenu";
-
+import CartComponent from "../components/menus/CartComponent";
+// im
 const BasicLayout = ({children}) => {
   return (
     <>
+      {/* 기존 헤더 대신 BasicMenu*/ }
       <BasicMenu></BasicMenu>
+
       {/* 상당 여백 my-5 제거 */}
       <div className="bg-white w-full flex flex-col space-y-1 my-5 md:flex-row md:space-x-4 md:space-y-0">
-        {/* 상단 여백 py-40 변경 */}
+        {/* 상단 여백 py-40 변경  flex 제거 */}
         <main className="bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5">
-          <h1 className="text-2xl md:text-4xl">{children}</h1>
+          {children}
         </main>
+
         {/* 상단 여백 py-40 제거 */}
         <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 py-5">
-          <h1 className="text-2xl md:text-4xl"> Sidebar </h1>
+          <CartComponent />
         </aside>
       </div>
     </>

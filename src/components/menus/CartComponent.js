2293288980d6b0adbec7c1869d6860b675ddb51e
beyondie2 <heewoong7843@gmail.com>
2025-05-30 12:24:23 +0900

    477 페이지 장바구니 아이템 가져오기

--- src/components/menus/CartComponent.js
+++ src/components/menus/CartComponent.js
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import useCustomLogin from "../../hooks/useCustomLogin";
+import { getCartItemsAsync } from "../../slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const CartComponent = () => {
+  const {isLogin, loginState} = useCustomLogin();
+  const dispatch = useDispatch()
+  const cartItems = useSelector(state => state.cartSlice)
+
+  useEffect(() => {
+    if(isLogin) {
+      dispatch(getCartItemsAsync())
+    }
+  },[isLogin])
+
   return (
     <div className="w-full">
-      <div>Cart</div>
+      {isLogin ?
+      <div className="flex">
+        <div className="m-2 font-extrabold">{loginState.nickname}'s Cart</div>
+        <div className="bg-orange-600 w-9 text-center text-white font-bold
+          rounded-full m-2">{cartItems.length}
+        </div>
+      </div>
+        
+      :
+        <div></div>
+      }
     </div>
   );
 }

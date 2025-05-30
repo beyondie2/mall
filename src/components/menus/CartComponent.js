3722a3fb5f653362e93d2f16611f4e51153a76bf
beyondie2 <heewoong7843@gmail.com>
2025-05-30 16:00:20 +0900

    481페이지 장바구니 아이템 출력

--- src/components/menus/CartComponent.js
+++ src/components/menus/CartComponent.js
@@ -1,31 +1,38 @@
 import { useEffect } from "react";
 import useCustomLogin from "../../hooks/useCustomLogin";
-import { getCartItemsAsync } from "../../slices/cartSlice";
-import { useDispatch, useSelector } from "react-redux";
+import useCustomCart from "../../hooks/useCustomCart";
+import CartItemComponent from "../cart/CartItemComponent";
 
 const CartComponent = () => {
   const {isLogin, loginState} = useCustomLogin();
-  const dispatch = useDispatch()
-  const cartItems = useSelector(state => state.cartSlice)
+  const {refreshCart, cartItems} = useCustomCart();
 
   useEffect(() => {
     if(isLogin) {
-      dispatch(getCartItemsAsync())
+      refreshCart()
     }
   },[isLogin])
 
   return (
     <div className="w-full">
       {isLogin ?
-      <div className="flex">
-        <div className="m-2 font-extrabold">{loginState.nickname}'s Cart</div>
-        <div className="bg-orange-600 w-9 text-center text-white font-bold
-          rounded-full m-2">{cartItems.length}
+
+      <div className="flex flex-col">
+
+        <div className="w-full flex">
+          <div className="font-extrabold text-2xl w-4/5">{loginState.nickname}'s Cart</div>
+          <div className="bg-orange-600 w-9 text-center text-white font-bold rounded-full m-1">{cartItems.length}</div>
         </div>
+
+        <div>
+          <ul>
+            {cartItems.map( item => <CartItemComponent {...item} key={item.cino}/>)}            
+          </ul>
+        </div>
+
       </div>
-        
       :
-        <div></div>
+      <div></div>
       }
     </div>
   );
